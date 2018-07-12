1b7f2b736dfb198990e906356ffbf6a595b681d3
bluebeadlily@gmail.com <bluebeadlily@gmail.com>
2018-07-12 02:58:10 +0000

    automatically backed up by learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,9 +5,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  select min(gpa) from students
 end
 
 def average_student_gpa
+  select ave(gpa) from students
 end
 
 def total_tardies_for_all_students
